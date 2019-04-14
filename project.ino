5642031f55e7994541c7a87d361e166362264de0
Ahmed Abdelfattah <ahmed.fattah98@eng-st.cu.edu.eg>
2019-04-14 23:52:28 +0200

    all without flame logic

--- project.ino
+++ project.ino
@@ -7,17 +7,21 @@ Servo gateExit;
 ////////////
 int gateEnterpin=3;
 int gateExitpin=5;
-int ledpins[]={,2,3,4,5};
-int flame=1;
+int ledpins[]={1,2,3,4,5};//will be modified
+int flamepin=1;
 int spotssensors[]={A2,A3,A4,A5};
 int entersensor=A0;
 int exitsensor=A1;
 int CounterSpots;
+int entergateangle;
+int exitgateangle;
+int detectionvalueGates;
+int detectionvalueSpots;
 ////////////////
 void setup() {
 gateEnter.attach(gateEnterpin);
 gateExit.attach(gateExitpin);
-pinMode(flame,INPUT);
+pinMode(flamepin,INPUT);
 pinMode(entersensor,INPUT);
 pinMode(exitsensor,INPUT);
 for(int i=0;i<4;i++)
@@ -28,85 +32,139 @@ for(int i=0;i<4;i++)
  
 }
  pinMode(ledpins[4],OUTPUT); 
- CounterSpots=0;  // used places
+ CounterSpots=0;  
+ entergateangle=1;
+ exitgateangle=1;
+ detectionvalueGates=1010;
+ detectionvalueSpots=1010;
+ lcd.clear();
+ lcd.setCursor(0,0);
+ lcd.print("HELLO PARKING");
+ delay(2000);
 }
+
+
 
 void loop() {
   
+CounterSpots=0;
+if(!carDetection(entersensor,detectionvalueGates))
+{
+  gateEnter.write(0);
+}
+if(!carDetection(exitsensor,detectionvalueGates))
+{
+  gateExit.write(0);
+  
+}
+  lcd.clear();  
+  lcd.setCursor(0,0); 
+  String AvailableSpots=""; 
   for(int i=0;i<4;i++)
   {
-    int sensorvalue=analogRead(spotssensors[i]);
-    if(sensorvalue<1010)
+    int sensorvalue=analogRead(spotssensors[i]);    
+    if(carDetection(spotssensors[i],detectionvalueSpots))
     {
       CounterSpots++;
+      digitalWrite(ledpins[i],HIGH);     
       
+    }
+    else{      
+           digitalWrite(ledpins[i],LOW);   
+           AvailableSpots+=String(i+1)+",";   
+      }     
+  }
+
+  if(CounterSpots<4)
+  {
+    
+    lcd.print("Available spots");
+    lcd.setCursor(1,0);    
+    lcd.print(AvailableSpots);
+    
+  }
+  else{   
+     lcd.print("Parking is FULL");    
     } 
-    
-  }
-//  show number of used "CounterSpots"
 
-  if(checkFlame())
-    {
-         flameLogic();
-    }
-  delay(100);
-
-   int entersensorval=analogRead(entersensor);
-   if(entersensorval<1010&&CounterSpots<4)
+   if(carDetection(entersensor,detectionvalueGates)&&CounterSpots<4)
    {
-
-    for(int i=1;i<=90;i*=10)
-    {
-        gateEnter.write(i);
-        if(checkFlame())
-          {
-               flameLogic();             
-              
-          }
-                
-      delay(100);
+        openEnterGate();
+   } 
+    
+    if(carDetection(exitsensor,detectionvalueGates)&&CounterSpots<4)
+   {
       
-    }      
-    
-   }
-    int exitsensorval=analogRead(exitsensor);   
-    if(exitsensorval<1010)
-   {
-
-    for(int i=1;i<=90;i*=10)
-    {
-        gateExit.write(i);               
-        delay(100);     
-    }  
+     openExitGate();
   
-   }
-  
-
-
-for(int i=0;i<4;i++){
-  
-  if(analogRead(spotssensors[i])<1010){
-    digitalWrite(ledpins[i],LOW);
-    
-    
-    }
-  else{
-      digitalWrite(ledpins[i],HIGH);
-    }
-  }
-
-  CounterSpots=0;
+   } 
 }
 
+
+
+void openEnterGate()
+{
+    for(entergateangle;entergateangle<=90;entergateangle+=10)
+    {
+      gateEnter.write(entergateangle);                
+      delay(100);
+      if(carDetection(exitsensor,detectionvalueGates))
+      {
+        openBoth();        
+      }
+      
+    }     
+     
+ } 
+
+void openExitGate()
+{
+  
+ for(exitgateangle;exitgateangle<=90;exitgateangle+=10)
+    {
+        gateExit.write(exitgateangle);               
+        delay(100);
+     if(carDetection(entersensor,detectionvalueGates))
+      {
+        openBoth();        
+      }     
+    }    
+   
+}
+
+void openBoth()
+{  
+
+while(exitgateangle<90||entergateangle<90)
+{
+  if(exitgateangle<90)
+  {
+    exitgateangle+=10;
+    gateExit.write(exitgateangle); 
+  }
+   if(entergateangle<90)
+  {
+    entergateangle+=10;
+    gateEnter.write(exitgateangle); 
+  } 
+  
+}  
+  
+}
+
+bool carDetection(int sensor,int detectvalue)
+{
+ return (analogRead(sensor)<detectvalue); 
+}
 
 
 bool checkFlame()
 {
- return (digitalRead(flame));    
+ return (digitalRead(flamepin));    
 }
 
 void flameLogic()
 {  
-
+//ba3deen b2a ;
   
 }
